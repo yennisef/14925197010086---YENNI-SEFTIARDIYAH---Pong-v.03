b88dd6d764f221c4dd3658229d64150bbabdbd39
yennisef <yongrhee1998@gmail.com>
2022-06-10 13:27:09 +0700

    init

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public Vector2 speed;
+    public Vector2 resetPosition;
+
+    private Rigidbody2D rig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        rig.velocity = speed;
+    }
+
+    public void ResetBall()
+    {
+        transform.position = new Vector3(resetPosition.x, resetPosition.y, 2);
+    }
+}
